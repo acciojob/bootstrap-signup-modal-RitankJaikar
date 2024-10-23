da1d9b3ef34cf8a30d7e0d26988cbfd4634a7485
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-24 01:53:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const exampleModal = document.getElementById('exampleModal')
 if (exampleModal) {
   exampleModal.addEventListener('show.bs.modal', event => {
-    const modalTitle = exampleModal.querySelector('.modal-title')
-    modalTitle.textContent = `Create Account`
+    //const modalTitle = exampleModal.querySelector('.modal-title')
+    //modalTitle.textContent = `Create Account`
   })
 
   // Add an event listener for when the modal is hidden
   exampleModal.addEventListener('hidden.bs.modal', () => {
-    console.log('Modal has been closed.')
-  })
+    console.log('Modal closed');
+  });
 }
